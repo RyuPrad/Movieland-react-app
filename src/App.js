6c32a89c079a78c9023f6ff5480bcd6ee0870c4b
Ryu Pradana <ryuprad@gmail.com>
2022-12-28 15:19:04 -0500

    minor changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 
 import './App.css'
 import SearchIcon from "./search.svg";
-const API_URL = "http://www.omdbapi.com?apikey=fa4fb641";
+const API_URL = "https://www.omdbapi.com?apikey=fa4fb641";
 
 const movie1 = {
     "Title": "Superman, Spiderman or Batman",
