8d2a0c2ff34c0a131be4c472ff4851661907d79b
Ryu Pradana <ryuprad@gmail.com>
2022-12-28 12:43:30 -0500

    Make the search bar dynamic

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ const movie1 = {
 
 const App = () => {
     const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
@@ -24,18 +25,21 @@ const App = () => {
         setMovies(data.Search);
     }
 
-    useEffect(() => {
-        searchMovies('Spiderman');
-    }, []);
+
 
     return (
         <div className="app">
             <h1>Movie Land</h1>
 
             <div className="search">
-                <input placeholder="Search for Movies" value="Superman"
-                    onChange={() => { }} />
-                <img src={SearchIcon} alt="search" onClick={() => {}}/>
+                <input
+                    placeholder="Search for Movies"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)} />
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => searchMovies(searchTerm)} />
             </div>
 
             {movies?.length > 0
