284df73759695b49e0dcb397616289176d091efe
Ryu Pradana <ryuprad@gmail.com>
2022-12-28 12:15:53 -0500

    testing by fetching a movie from api

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import './App.css'
 import SearchIcon from "./search.svg";
 const API_URL = "http://www.omdbapi.com?apikey=fa4fb641";
 
+const movie1 = {
+    "Title": "Superman, Spiderman or Batman",
+    "Year": "2011",
+    "imdbID": "tt2084949",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg"
+}
+
 const App = () => {
 
     const searchMovies = async (title) => {
@@ -23,7 +31,26 @@ const App = () => {
 
             <div className="search">
                 <input placeholder="Search for Movies" value="Superman"
-                onChange={() => {}}/>
+                    onChange={() => { }} />
+                <img src={SearchIcon} alt="search" onClick={() => {}}/>
+            </div>
+
+            <div className="container">
+                <div className="movie">
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+
+                    <div>
+                        <img src={movie1.Poster !== "N/A" ? movie1.Poster :
+                        "https://via.placeholder.com/400"} alt={movie1.Title} />
+                    </div>
+
+                    <div>
+                        <span>{movie1.Type}</span>
+                        <h3>{movie1.Title}</h3>
+                    </div>
+                </div>
             </div>
         </div>
     );
