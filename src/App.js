7327d3a0d7c09fbcb76669a1ecb5e585ae867dee
Ryu Pradana <ryuprad@gmail.com>
2022-12-28 12:34:58 -0500

    Map over the api and display movies in the browser

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { useEffect } from "react"; 
+import { useEffect, useState } from "react"; 
+
+import MovieCard from "./MovieCard";
 
 import './App.css'
 import SearchIcon from "./search.svg";
@@ -13,12 +15,13 @@ const movie1 = {
 }
 
 const App = () => {
+    const [movies, setMovies] = useState([]);
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
 
-        console.log(data.Search);
+        setMovies(data.Search);
     }
 
     useEffect(() => {
@@ -35,24 +38,20 @@ const App = () => {
                 <img src={SearchIcon} alt="search" onClick={() => {}}/>
             </div>
 
-            <div className="container">
-                <div className="movie">
-                    <div>
-                        <p>{movie1.Year}</p>
-                    </div>
-
-                    <div>
-                        <img src={movie1.Poster !== "N/A" ? movie1.Poster :
-                        "https://via.placeholder.com/400"} alt={movie1.Title} />
-                    </div>
-
-                    <div>
-                        <span>{movie1.Type}</span>
-                        <h3>{movie1.Title}</h3>
-                    </div>
+            {movies?.length > 0
+                ? (
+                <div className="container">
+                    {movies.map((movie) => (
+                        <MovieCard movie={movie} />
+                    ))}
                 </div>
+                ) :
+                (
+                <div className="empty">
+                    <h2>No movies found</h2>
+                </div>
+                )}
             </div>
-        </div>
     );
 }
 
