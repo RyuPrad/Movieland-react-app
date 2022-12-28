088f29dbbb9d6fe037ece68a85cd79006a392ca0
Ryu Pradana <ryuprad@gmail.com>
2022-12-27 22:46:11 -0500

    Added app.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from "react"; 
+import { useEffect } from "react"; 
 
 const API_URL = "http://www.omdbapi.com?apikey=fa4fb641"
 
 const App = () => {
+
+
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.search);
+    }
+
+    useEffect(() => {
+        searchMovies('Spiderman')
+    }, []);
+
     return (
         <h1>App</h1>
     );
